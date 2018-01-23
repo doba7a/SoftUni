963f7dfa8941bd709f8105b951af65a3c0111fcd
doba7a <grizzlie@abv.bg>
2018-01-23 23:16:21 +0200

    Added the homework.

---
+++ CSharp Fundamentals/CSharp Advanced/2. Stacks And Queues - Exercise/11. PoisonousPlants/PoisonousPlants.cs
@@ -0,0 +1,44 @@
+﻿namespace PoisonousPlants
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PoisonousPlants
+    {
+        public static void Main()
+        {
+            int numberOfPlants = int.Parse(Console.ReadLine());
+
+            List<long> plantsPesticide = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToList();
+
+            int[] days = new int[numberOfPlants];
+
+            Stack<int> indexesToRemove = new Stack<int>();
+            indexesToRemove.Push(0);
+
+            for (int i = 1; i < numberOfPlants; i++)
+            {
+                int maxDays = 0;
+
+                while (indexesToRemove.Count > 0 
+                    && plantsPesticide[indexesToRemove.Peek()] >= plantsPesticide[i])
+                {
+                    maxDays = Math.Max(maxDays, days[indexesToRemove.Pop()]);
+                }
+
+                if (indexesToRemove.Count > 0)
+                {
+                    days[i] = maxDays + 1;
+                }
+
+                indexesToRemove.Push(i);
+            }
+
+            Console.WriteLine(days.Max());
+        }
+    }
+}
---
+++ CSharp Fundamentals/CSharp Advanced/2. Stacks And Queues - Exercise/7. BalancedParantheses/BalancedParantheses.cs
@@ -0,0 +1,44 @@
+﻿namespace BalancedParantheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParantheses
+    {
+        public static void Main()
+        {
+            Stack<char> openingBrackets = new Stack<char>();
+            Queue<char> closingBrackets = new Queue<char>();
+
+            string input = Console.ReadLine();
+
+            foreach (char paranthes in input)
+            {
+                if (paranthes == '[' || paranthes == '{' || paranthes == '(')
+                {
+                    openingBrackets.Push(paranthes);
+                }
+                else if (paranthes == ']' || paranthes == '}' || paranthes == ')')
+                {
+                    if (openingBrackets.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        Environment.Exit(1);
+                    }
+
+                    char lastOpeningBracket = openingBrackets.Pop();
+
+                    if (lastOpeningBracket == '[' && paranthes != ']'
+                        || lastOpeningBracket == '{' && paranthes != '}'
+                        || lastOpeningBracket == '(' && paranthes != ')')
+                    {
+                        Console.WriteLine("NO");
+                        Environment.Exit(1);
+                    }
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
