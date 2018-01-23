6c7e82e24f8b06f6e787536b68dd974179fb885d
doba7a <grizzlie@abv.bg>
2018-01-23 14:35:08 +0200

    Added the homework.

---
+++ CSharp Fundamentals/CSharp Advanced/1. Stacks And Queues - Lab/3. DecimalToBinaryConverter/DecimalToBinaryConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace DecimalToBinaryConverter
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DecimalToBinaryConverter
+    {
+        public static void Main()
+        {
+            int decimalInput = int.Parse(Console.ReadLine());
+
+            if (decimalInput == 0)
+            {
+                Console.WriteLine(0);
+                Environment.Exit(1);
+            }
+
+            Stack<byte> stack = new Stack<byte>();
+
+            while (decimalInput > 0)
+            {
+                byte remainder = (byte)(decimalInput % 2);
+
+                stack.Push(remainder);
+
+                decimalInput /= 2;
+            }
+
+            Console.WriteLine(string.Join("", stack));
+        }
+    }
+}
