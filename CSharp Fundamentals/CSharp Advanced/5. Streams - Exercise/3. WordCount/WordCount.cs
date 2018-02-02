be1bb2fcc6390c39268b1265ba871145e4ad4b3d
doba7a <grizzlie@abv.bg>
2018-02-02 19:03:24 +0200

    Added the homework.

---
+++ CSharp Fundamentals/CSharp Advanced/5. Streams - Exercise/3. WordCount/WordCount.cs
@@ -0,0 +1,97 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class WordCount
+    {
+        public static void Main()
+        {
+            Dictionary<string, int> wordsCountDict = new Dictionary<string, int>();
+
+            Console.Write("Enter words file directory and name with extension:");
+            string directoryAndName = Console.ReadLine();
+
+            AddWords(directoryAndName, wordsCountDict);
+
+            Console.Write("Enter text file directory and name with extension:");
+            directoryAndName = Console.ReadLine();
+
+            CountWords(directoryAndName, wordsCountDict);
+
+            Result(wordsCountDict);
+        }
+
+        public static void AddWords(string directoryAndName, Dictionary<string, int> wordsCountDict)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(directoryAndName))
+                {
+                    string word = sr.ReadLine().ToLower();
+
+                    while (word != null)
+                    {
+                        wordsCountDict[word] = 0;
+
+                        word = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid path or file name. Press any key to exit.");
+                Environment.Exit(0);
+            }
+        }
+
+        public static void CountWords(string directoryAndName, Dictionary<string, int> wordsCountDict)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(directoryAndName))
+                {
+                    string line = sr.ReadLine();
+
+                    while (line != null)
+                    {
+                        string[] words = line
+                            .Split(new[] { ' ', ',', '.', '-', '!', '?' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(w => w.ToLower())
+                            .ToArray();
+
+                        foreach (string word in words)
+                        {
+                            if (wordsCountDict.ContainsKey(word))
+                            {
+                                wordsCountDict[word]++;
+                            }
+                        }
+
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid path or file name. Press any key to exit.");
+                Environment.Exit(0);
+            }
+        }
+
+        public static void Result(Dictionary<string, int> wordsCountDict)
+        {
+            using (StreamWriter sw = new StreamWriter("result.txt"))
+            {
+                foreach (KeyValuePair<string, int> kvp in wordsCountDict.OrderByDescending(x => x.Value))
+                {
+                    sw.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+            }
+
+            Console.WriteLine("Result added to result.txt in project folder. Press any key to exit.");
+        }
+    }
+}
---
+++ CSharp Fundamentals/CSharp Advanced/5. Streams - Exercise/4. CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,49 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        public static void Main()
+        {
+            Console.Write("Enter binary file directory and name with extension:");
+            string directoryAndName = Console.ReadLine();
+            string fileExtension = directoryAndName.Substring(directoryAndName.LastIndexOf('.') + 1);
+
+            CopyFile(directoryAndName, fileExtension);
+        }
+
+        public static void CopyFile(string directoryAndName, string fileExtension)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(directoryAndName, FileMode.Open))
+                using (FileStream destination = new FileStream($"copied.{fileExtension}", FileMode.Create))
+                {
+                    byte[] buffer = new byte[fs.Length];
+
+                    while (true)
+                    {
+                        int readBytes = fs.Read(buffer, 0, buffer.Length);
+
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+
+                        destination.Write(buffer, 0, readBytes);
+                    }
+
+                    Console.WriteLine($"Result added to copied.{fileExtension} in project folder. Press any key to exit.");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid path or file name. Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
