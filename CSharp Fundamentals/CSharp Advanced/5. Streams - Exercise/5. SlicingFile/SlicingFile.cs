b7c0b86a090a3bef3853007a6046eb3abe64aa8e
doba7a <grizzlie@abv.bg>
2018-02-02 19:06:28 +0200

    Added the homework.

---
+++ CSharp Fundamentals/CSharp Advanced/5. Streams - Exercise/5. SlicingFile/SlicingFile.cs
@@ -0,0 +1,142 @@
+﻿namespace SlicingFile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class SlicingFile
+    {
+        public static void Main()
+        {
+            Console.Write("Type 1 to *SLICE* a file or 2 to *ASSEMBLE* file: ");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                try
+                {
+                    Console.Write("Enter source file directory and name with extension: ");
+                    string directoryAndName = Console.ReadLine();
+
+                    Console.Write("Enter directory for sliced files: ");
+                    string destionationDirectory = Console.ReadLine();
+
+                    Console.Write("Enter number of slices: ");
+                    int parts = int.Parse(Console.ReadLine());
+
+                    Slice(directoryAndName, destionationDirectory, parts);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You have entered invalid data. Please try again!");
+                }
+                
+            }
+            else if (option == "2")
+            {
+                List<string> files = new List<string>();
+
+                Console.Write("Enter file directory and name with extension to be assembled or press *ENTER* if you won't add more files: ");
+                string file = Console.ReadLine();
+                while (file != string.Empty)
+                {
+                    files.Add(file);
+
+                    Console.Write("Enter file directory and name with extension to be assembled or press *ENTER* if you won't add more files: ");
+                    file = Console.ReadLine();
+                }
+
+                Console.Write("Enter directory for assembled file: ");
+                string destionationDirectory = Console.ReadLine();
+
+                Console.Write("Enter name for assembled file: ");
+                string fileName = Console.ReadLine();
+
+                try
+                {
+                    Assemble(files, destionationDirectory, fileName);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You have entered invalid data. Please try again");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again");
+            }
+        }
+
+        public static void Slice(string sourceFile, string destinationDirectory, int parts)
+        {
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+
+            using (FileStream fsIN = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
+            {
+                int sizeOfEachFile = (int)Math.Ceiling((double)fsIN.Length / parts);
+
+                string fileName = sourceFile.Substring(0, sourceFile.LastIndexOf('.'));
+
+                string extension = sourceFile.Substring(sourceFile.LastIndexOf('.') + 1);
+
+                for (int i = 1; i <= parts; i++)
+                {
+                    string outFilepath = destinationDirectory + "\\" + fileName + $"Part{i}." + extension;
+
+                    using (FileStream fsOUT = new FileStream(outFilepath, FileMode.Create, FileAccess.Write))
+                    {
+                        int bytesRead = 0;
+                        byte[] buffer = new byte[sizeOfEachFile];
+
+                        if ((bytesRead = fsIN.Read(buffer, 0, sizeOfEachFile)) > 0)
+                        {
+                            fsOUT.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Slicing done.");
+        }
+
+        public static void Assemble(List<string> files, string destinationDirectory, string fileName)
+        {
+            if (files.Count <= 0)
+            {
+                Console.WriteLine("No input files specified!");
+                return;
+            }
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+
+            var fileExtension = Path.GetExtension(files.First());
+
+            var path = destinationDirectory + "\\" + fileName + fileExtension;
+
+            using (FileStream fsOUT = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                foreach (string filePath in files)
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = 0;
+
+                    using (FileStream inpFile = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        while ((bytesRead = inpFile.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fsOUT.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Files Merged");
+        }
+    }
+}
---
+++ CSharp Fundamentals/CSharp Advanced/5. Streams - Exercise/7. DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,59 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class DirectoryTraversal
+    {
+        public static void Main()
+        {
+            Console.Write("Enter directory: ");
+            string searchedDirectory = Console.ReadLine();
+            DirectoryInfo directorySelected = new DirectoryInfo(searchedDirectory);
+
+            SortedDictionary<string, Dictionary<string, double>> extensions = new SortedDictionary<string, Dictionary<string, double>>();
+            FilesExtensions(directorySelected, extensions);
+
+            using (StreamWriter writer = new StreamWriter("extensions.txt"))
+            {
+                WritingOnFile(writer, extensions);
+            }
+        }
+
+        private static void WritingOnFile(StreamWriter writer, SortedDictionary<string, Dictionary<string, double>> extensions)
+        {
+            foreach (var extension in extensions
+               .OrderByDescending(extension => extension.Value.Count)
+               .ThenBy(extension => extension.Key))
+            {
+
+                writer.WriteLine(extension.Key);
+                var orderedDic = extension.Value.OrderBy(dic => dic.Value);
+                foreach (var dic in orderedDic)
+                {
+                    writer.WriteLine("{0}{1:F2}kb", dic.Key, dic.Value / 1024);
+                }
+            }
+
+            Console.WriteLine("Result added to extensions.txt at project folder");
+        }
+
+        private static void FilesExtensions(DirectoryInfo directorySelected, SortedDictionary<string, Dictionary<string, double>> extensions)
+        {
+            foreach (var file in directorySelected.GetFiles())
+            {
+                if (!extensions.ContainsKey(file.Extension))
+                {
+                    extensions.Add(file.Extension, new Dictionary<string, double> { { string.Format("--{0} - ", file.Name), file.Length } });
+                }
+                else
+                {
+                    extensions[file.Extension].Add(string.Format("--{0} - ", file.Name), file.Length);
+                }
+
+            }
+        }
+    }
+}
